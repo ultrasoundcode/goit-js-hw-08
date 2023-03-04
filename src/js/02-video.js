1a9b0e0b7d1e6ddbc04f6ccc9b7ddf64a199e0b1
Askhat Anarbayev <0x.ultrasound@gmail.com>
2023-03-04 14:53:41 +0600

    02-video.js ✅

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
