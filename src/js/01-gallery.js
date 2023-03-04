212fdf1c41435f7f9e779ce70f4ef241649a7134
Askhat Anarbayev <0x.ultrasound@gmail.com>
2023-03-04 13:07:00 +0600

    gallery done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMurkUp = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMurkUp);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class='gallery__item'>
+								<a class='gallery__link'  href="${original}">
+									<img class='gallery__image' src="${preview}" data-source="${original}" alt="${description}">
+									</img>
+								</a>
+							</div>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
